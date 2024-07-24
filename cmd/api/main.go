46b9161c5a947c8d0951d18d884eef5009c0eb8a
ruthwik-t <ruthwik2525@gmail.com>
2024-07-25 05:21:10 +0530

    add database config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/ruthwik-t/social/internal/db"
 	"github.com/ruthwik-t/social/internal/env"
 	"github.com/ruthwik-t/social/internal/store"
 )
@@ -10,9 +11,25 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_DB_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_DB_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	storage := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	storage := store.NewStorage(db)
 
 	app := &application{
 		config:  cfg,
